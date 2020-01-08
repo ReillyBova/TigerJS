a0ccdbc91fa7d11e68a52b7b1a089a2f0437cfc4
Reilly Bova <rbova@princeton.edu>
2020-01-08 03:01:30 -0500

    Updates

---
+++ precache-manifest.37cbe8377a01289cc6580956f436fe81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68f336b10bd3560c590c6f54066c3b7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "795af464f7817bf6f515",
+    "url": "/static/js/2.3d3c9098.chunk.js"
+  },
+  {
+    "revision": "f63c345413798f3792d990f17ddf45d5",
+    "url": "/static/js/2.3d3c9098.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7c62fe36f6b30c0b78d",
+    "url": "/static/js/main.b806aed4.chunk.js"
+  },
+  {
+    "revision": "6ea18c12d67c75f88eab",
+    "url": "/static/js/runtime-main.c584c3ff.js"
+  }
+]);
